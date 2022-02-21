d363c799e2f7a72aa81d70ab81579fef4ac4aec9
Heithvald <molostov.inc@gmail.com>
2022-02-21 11:59:55 +0300

    add router and audit fix, add local images, add HOC withrouter for menuItem, add directory and sections data files

--- src/components/directory-item.component.tsx
+++ src/components/directory-item.component.tsx
@@ -1,5 +1,6 @@
 import { DirectorySection } from 'models/directory-section.model'
 import React from 'react'
+import SECTIONS_DATA from 'sections.data'
 import MenuItem from './menu-item.component'
 
 type DirectoryState = {
@@ -13,40 +14,7 @@ export default class Directory extends React.Component<
 	constructor(props: unknown) {
 		super(props)
 		this.state = {
-			sections: [
-				{
-					title: 'hats',
-					imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-					id: 1,
-					linkUrl: 'shop/hats',
-				},
-				{
-					title: 'jackets',
-					imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-					id: 2,
-					linkUrl: 'shop/jackets',
-				},
-				{
-					title: 'sneakers',
-					imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-					id: 3,
-					linkUrl: 'shop/sneakers',
-				},
-				{
-					title: 'womens',
-					imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-					size: 'large',
-					id: 4,
-					linkUrl: 'shop/womens',
-				},
-				{
-					title: 'mens',
-					imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-					size: 'large',
-					id: 5,
-					linkUrl: 'shop/mens',
-				},
-			],
+			sections: SECTIONS_DATA,
 		}
 	}
 
@@ -54,8 +22,8 @@ export default class Directory extends React.Component<
 		const { sections } = this.state
 		return (
 			<div className='directory-menu flex flex-wrap space-between w-full'>
-				{sections.map(({ title, id, imageUrl }) => (
-					<MenuItem title={title} imageUrl={imageUrl} key={id} />
+				{sections.map(({ id, ...otherProps }) => (
+					<MenuItem key={id} {...otherProps} />
 				))}
 			</div>
 		)
--- src/components/menu-item.component.tsx
+++ src/components/menu-item.component.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import { RouteComponentProps, withRouter } from 'react-router-dom'
 
 interface MenuItemProps {
 	title: string
 	imageUrl: string
+	linkUrl: string
 }
 
-export default function MenuItem(props: MenuItemProps) {
-	const { title, imageUrl } = props
+function MenuItem(props: MenuItemProps & RouteComponentProps): JSX.Element {
+	const { title, imageUrl, linkUrl, history, match } = props
 	return (
-		<div
+		<button
+			type='button'
 			className='group menu-item min-w-30-perc flex-auto h-60 flex justify-center 
                 cursor-pointer items-center border mt-0 mr-3 mb-7 first:mr-3 relative overflow-hidden'
+			// open concrete section
+			onClick={() => history.push(`${match.url}${linkUrl}`)}
 		>
 			<div
 				className='w-full h-full bg-center bg-cover absolute group-hover:scale-110 transition-transform duration-700'
@@ -18,10 +23,12 @@ export default function MenuItem(props: MenuItemProps) {
 					backgroundImage: `url(${imageUrl})`,
 				}}
 			/>
-			<div className='content border p-5 bg-gray-100 bg-opacity-60 hover:bg-opacity-80 transition z-10'>
+			<div className='content border border-gray-400 p-5 bg-gray-100 bg-opacity-60 hover:bg-opacity-80 transition z-10'>
 				<h1 className='title font-semibold'>{title.toUpperCase()}</h1>
 				<span className='subtitle'>SHOP NOW</span>
 			</div>
-		</div>
+		</button>
 	)
 }
+
+export default withRouter(MenuItem)
